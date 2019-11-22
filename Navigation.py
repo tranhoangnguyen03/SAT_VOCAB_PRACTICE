7983d9d9d8220933dccfecab81cb49d8423e829a
tranhoangnguyen03 <31383641+tranhoangnguyen03@users.noreply.github.com>
2019-11-22 17:09:56 +0700

    Update Navigation.py

--- Navigation.py
+++ Navigation.py
@@ -1,8 +1,8 @@
-import os, requests, re, time, datetime, pandas_gbq, getpass, uuid 
+import os, requests, re, time, datetime, pandas_gbq, getpass, json, IPython, uuid
 import numpy as np
-from uuid import uuid4
-from google.oauth2 import service_account
 import pandas as pd
+from google.oauth2 import service_account
+from google.colab import output
 from IPython.display import clear_output
 
 class Navigation_simple:
