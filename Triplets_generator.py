8bb0197cf1e320eafa186fb3e72212c586e4d184
tranhoangnguyen03 <31383641+tranhoangnguyen03@users.noreply.github.com>
2019-11-22 17:02:10 +0700

    Update Triplets_generator.py

--- Triplets_generator.py
+++ Triplets_generator.py
@@ -1,27 +1,33 @@
-import os, requests, re, time, datetime, pandas_gbq, getpass
+import os, requests, re, time, datetime, pandas_gbq, getpass, json
 import numpy as np
 from google.oauth2 import service_account
 import pandas as pd
 from IPython.display import clear_output
 
 class MainEngine:
-  def __init__(self, vocab_file):
+  def __init__(self, Selection, wordFam):
     class _format_:
       GREEN = '\033[92m'
       BOLD = '\033[1m'
       UNDERLINE = '\033[4m'
       RESET = '\033[0m'
     self.format = _format_
-    if not os.path.isfile(vocab_file): os.system(f'gsutil cp gs://sat_vocab_test/Vocab_txt/{vocab_file} .')
-    assert os.path.isfile(vocab_file), 'Vocab file not found!' 
-    with open(vocab_file,'r') as file:
-      self.string_ = file.read()
-    self.string_ = self.string_.strip('\n').split('''\n\n''')
+    self.wordFam = wordFam
+    self.string_ = Selection.string_.strip('').split('''\n\n''')
+    self.vocab_file = Selection.choice
     self.words_bank = [i[:i.find('-')-1].replace('TO ','').replace('to ','').lower().strip('') for i in self.string_]  
     self.words_bank_test = [i[:i.find(')')+1].replace('TO ','').replace('to ','').lower().strip('') for i in self.string_]
+    if Selection.from_to != None:
+      from_ = Selection.from_to[0]
+      to_ = Selection.from_to[1]+1
+      self.words_bank = self.words_bank[from_:to_]
+      self.words_bank_test = self.words_bank_test[from_:to_]
+      self.vocab_file = self.vocab_file+f'_{from_}_{to_}'
     self.used_triplets = []
-    self.used_triplets_test = []
-    self.vocab_file = vocab_file
+    self.used_triplets_test = []    
+
+  def Export(self):
+    self.present(self.words_bank)
 
   def Practice(self):
     flag_exit = False
@@ -84,18 +90,55 @@ class MainEngine:
       print(f"""{self.format.BOLD}{dict_['Word'].upper()}{self.format.RESET} ({dict_['Form']}) - {dict_['Definition']}
     EXAMPLE: {dict_['Example']}
     Synonyms: {dict_['Synonyms']}
+    Word Families: {sorted(self.wordFam.return_fam(word))}
               ----------------------""")
     print(f"{self.format.UNDERLINE}{self.format.BOLD}MAKE YOUR SENTENCE:{self.format.RESET}")
     # """
     return None
 
   def score(self, words, sentence):
+    words = self.wordFam.flatten([self.wordFam.return_fam(word) for word in words])
     return np.sum([i[:i.find('-')].replace('TO ','').replace('to ','').strip(' ').upper() in sentence.upper() for i in words])
 
 class WordFam:
-  import json
   def __init__(self,file):
     with open(file,'r') as f:
       self.wf = json.load(f)
+  
   def return_fam(self, word):
-    return 'something'
+    return self.flatten([self.wf[i] for i, group in enumerate(self.wf) if word in group])
+  
+  def flatten(self, S):
+    if S == []:
+      return S
+    if isinstance(S[0], list):
+      return self.flatten(S[0]) + self.flatten(S[1:])
+    return S[:1] + self.flatten(S[1:])  
+
+class Vocab_select:
+  def __init__(self):
+    self.files_dict = {i[i.rfind('/')+1:]:i for i in os.popen('gsutil ls gs://sat_vocab_test/Vocab_txt/SAT-400').read().split('\n')[1:-1]}
+    self.from_to = None
+  
+  def GetFiles(self, a):
+    output.clear()
+    self.choice = a
+    if not os.path.isfile(a): os.system(f'gsutil cp {file_dict[a]} .')
+    assert os.path.isfile(a), 'Vocab file not found!'
+    with open(a,'r') as file:
+      self.string_ = file.read()
+    if a != '999_vocab_SAT_ALL.txt':
+      print('''
+--------------------------------------------------------------------------------
+              Vocab file loaded! Please continue below!''')
+    else: 
+      print('Please input your custom word range:')
+      display(ReceiveInput('Input format: [start]-[end]', self.GetCustomFiles))
+      print('''
+--------------------------------------------------------------------------------
+              Vocab file loaded! Please continue below!''')
+  
+  def GetCustomFiles(self, value):
+    output.clear()
+    self.from_to = (int(value[:value.find('-')]),int(value[value.find('-')+1:]))
+    print(f'Custom Vocab list created from word[{self.from_to[0]}] to word[{self.from_to[1]}]')    
