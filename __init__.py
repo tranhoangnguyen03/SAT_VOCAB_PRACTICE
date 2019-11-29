54dca3ddf4141638c731400e75c38d7444ac3be5
tranhoangnguyen03 <31383641+tranhoangnguyen03@users.noreply.github.com>
2019-11-29 12:53:15 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
-import os, requests, re, time, datetime, pandas_gbq, getpass, json, IPython, uuid
+import os, requests, re, time, datetime, pandas_gbq, getpass, json, IPython, uuid, google.cloud.storage
 import numpy as np
 import pandas as pd
+from bs4 import BeautifulSoup
 from google.oauth2 import service_account
 from google.colab import output
 from IPython.display import clear_output
