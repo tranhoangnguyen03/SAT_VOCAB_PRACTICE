494f1fedc733665536848883d693b8e0a413db39
tranhoangnguyen03 <31383641+tranhoangnguyen03@users.noreply.github.com>
2019-11-29 13:08:59 +0700

    Update Triplets_generator.py

--- Triplets_generator.py
+++ Triplets_generator.py
@@ -77,7 +77,7 @@ class MainEngine:
     url = f'https://www.google.com/search?hl=en&q={word}'
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'}
     response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.content).find("div", class_= "VpH2eb vmod XpoqFe")
+    soup = BeautifulSoup(response.content, "html.parser" ).find("div", class_= "VpH2eb vmod XpoqFe")
     if not soup:
       url = f'https://wordsapiv1.p.mashape.com/words/{word}'
       headers = {'x-rapidapi-host': "wordsapiv1.p.rapidapi.com",
